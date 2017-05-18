8b92502b72602e0d240732658c5fed94733a371f
root <root@ip-172-31-15-55.us-west-2.compute.internal>
2017-05-18 15:48:53 -0400

    sample.sh

---
+++ sample.sh
@@ -0,0 +1,12 @@
+echo "enter file name:"
+read FILE
+
+if [ -f $FILE ]
+then 
+	echo "file $FILE exist"
+git add $FILE
+git commit -m $FILE
+git push mygithub1
+else
+	echo "file $FILE does not exist"
+fi
